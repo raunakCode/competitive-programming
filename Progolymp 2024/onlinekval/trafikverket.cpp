5ff10f8bf25ac03b4adb9ee6d4f9f2c13974311f
Raunak Redkar <raunak.redkar@gmail.com>
2023-11-27 16:20:28 +0100

    fixing indentation

--- Progolymp 2024/onlinekval/trafikverket.cpp
+++ Progolymp 2024/onlinekval/trafikverket.cpp
@@ -36,92 +36,92 @@ vi depth;
 vector<vpii> up;
 
 void precomp(int node, int parent, int d) {
-    par[node] = parent;
-    depth[node] = d;
-    trav(child, graph[node]) {
-        if (child != parent) precomp(child, node, d+1);
-    }
+    	par[node] = parent;
+    	depth[node] = d;
+    	trav(child, graph[node]) {
+        	if (child != parent) precomp(child, node, d+1);
+    	}
 }
 
 int get_lca(int a, int b) {
         if (depth[a] < depth[b]) {
-        swap(a, b);
+        	swap(a, b);
         }
         int k = depth[a] - depth[b];
         for(int i = 20; i >= 0; i--) {
-        if (k & (1<<i)) {
-                a = up[a][i].F;
-        }
+		if (k & (1<<i)) {
+			a = up[a][i].F;
+		}
         }
         if (a == b) return a;
         for(int i = 20; i >= 0; i--) {
-        if (up[a][i].F != up[b][i].F) {
-                a = up[a][i].F;
-                b = up[b][i].F;
-        }
+		if (up[a][i].F != up[b][i].F) {
+			a = up[a][i].F;
+			b = up[b][i].F;
+		}
         }
         a = up[a][0].F;
         return a;
 }
 
 void solve() {
-    int n; cin >> n;
-    graph.resize(n);
-    par.resize(n);
-    depth.resize(n);
-    up.resize(n, vpii(21, mp(0, 0)));
-    fast_set<pii> u;
-    rep(i, 0, n-1) {
-        int a, b;
-        cin >> a >> b; a--, b--;
-        graph[a].pb(b);
-        graph[b].pb(a);
-        u.insert(mp(a, b));
-    }
+    	int n; cin >> n;
+    	graph.resize(n);
+    	par.resize(n);
+    	depth.resize(n);
+    	up.resize(n, vpii(21, mp(0, 0)));
+    	fast_set<pii> u;
+    	rep(i, 0, n-1) {
+		int a, b;
+		cin >> a >> b; a--, b--;
+		graph[a].pb(b);
+		graph[b].pb(a);
+		u.insert(mp(a, b));
+    	}
 
-    precomp(0, 0, 0);
+    	precomp(0, 0, 0);
 
-    rep(node, 0, n) {
-        int p = par[node];
-        pii edge = mp(p, node);
-        up[node][0].F = p;
-        if (u.find(edge) != u.end()) {
-            up[node][0].S = 1;
-        }
-    }
-    rep(k, 1, 21) {
-        rep(node, 0, n) {
-            up[node][k].F = up[up[node][k-1].F][k-1].F;
-            up[node][k].S = up[node][k-1].S + up[up[node][k-1].F][k-1].S;
-        }
-    }
+    	rep(node, 0, n) {
+		int p = par[node];
+		pii edge = mp(p, node);
+		up[node][0].F = p;
+		if (u.find(edge) != u.end()) {
+		    	up[node][0].S = 1;
+		}
+    	}
+    	rep(k, 1, 21) {
+		rep(node, 0, n) {
+		    	up[node][k].F = up[up[node][k-1].F][k-1].F;
+		    	up[node][k].S = up[node][k-1].S + up[up[node][k-1].F][k-1].S;
+		}
+    	}
 
-    int q; cin >> q;
-    while(q--) {
-        int h, w;
-        cin >> h >> w; h--, w--;
-        int lca = get_lca(h, w);
-        int lca_depth = depth[lca];
-        int workDist = depth[w] - lca_depth;
-        int homeLca = 0, workLca = 0;
-        for(int k = 20; k >= 0; k--) {
-            int nxt = up[h][k].F;
-            if (depth[nxt] > lca_depth) {
-                homeLca += up[h][k].S;
-                h = up[h][k].F;
-            }
-        }
-        if (h != lca) homeLca += up[h][0].S;
-        for(int k = 20; k >= 0; k--) {
-            int nxt = up[w][k].F;
-            if (depth[nxt] > lca_depth) {
-                workLca += up[w][k].S;
-                w = up[w][k].F;
-            }
-        }
-        if (w != lca) workLca += up[w][0].S;
-        cout << (homeLca == 0 && workLca == workDist ? "ja" : "nej") << el;
-    }
+    	int q; cin >> q;
+    	while(q--) {
+		int h, w;
+		cin >> h >> w; h--, w--;
+		int lca = get_lca(h, w);
+		int lca_depth = depth[lca];
+		int workDist = depth[w] - lca_depth;
+		int homeLca = 0, workLca = 0;
+		for(int k = 20; k >= 0; k--) {
+		    	int nxt = up[h][k].F;
+		    	if (depth[nxt] > lca_depth) {
+				homeLca += up[h][k].S;
+				h = up[h][k].F;
+		    	}
+		}
+		if (h != lca) homeLca += up[h][0].S;
+		for(int k = 20; k >= 0; k--) {
+		    	int nxt = up[w][k].F;
+		    	if (depth[nxt] > lca_depth) {
+				workLca += up[w][k].S;
+				w = up[w][k].F;
+		    	}
+		}
+		if (w != lca) workLca += up[w][0].S;
+		cout << (homeLca == 0 && workLca == workDist ? "ja" : "nej") << el;
+    	}
 }
 
 int main() {
